7ad1dd6f2ff974d5002f3ee4752e6368db6878a9
Rohim Alaa <rohimalaa1@gmail.com>
2026-03-17 00:06:51 +0200

    update

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import cors from "cors";
 import  dotenv from 'dotenv';
 dotenv.config()
 const app = express()
-const port = process.env.PORT
+const port = process.env.PORT||3009
 app.use(express.json())
 app.use(cors());
 app.use('/users',userRouter)
